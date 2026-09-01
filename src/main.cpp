34e105ef4902c220fe722bec770a3628ed82b8bd
oktaavsm <incdmincs@gmail.com>
2026-09-01 12:01:38 +0700

    experiment: orthographic projection - chessboard wkwk

--- src/main.cpp
+++ src/main.cpp
@@ -25,21 +25,66 @@ void display()
     // ==========================================
     // Rendering goes here
     // ==========================================
+    int size = 25;
+    for(int i = 0; i < 8 ; i++){
+        for (int j=0; j < 8; j++){
+            if((i+j)%2==0){
+                glColor3f(1.0f, 1.0f, 1.0f);
+            }else{
+                glColor3f(0.0f, 0.0f, 0.0f);
+            }
+            glBegin(GL_POLYGON);
+                glVertex3f(i*size, j*size, 0.0);
+                glVertex3f(i*size, (j+1)*size, 0.0);
+                glVertex3f((i+1)*size, (j+1)*size, 0.0);
+                glVertex3f((i+1)*size, j*size, 0.0);
+            glEnd();
+        }
+    }
+    // // Kotak Merah di Z = 0.0 (Posisi normal)
+    // glColor3f(0.0f, 0.0f, 0.0f);
+    // glBegin(GL_POLYGON);
+    //     glVertex3f(-20.0, -20.0,   0.0);
+    //     glVertex3f(-20.0,  20.0,   0.0);
+    //     glVertex3f( 80.0,  80.0,   0.0);
+    //     glVertex3f( 80.0, -20.0,   0.0);
+    // glEnd();
+
+    // // Kotak Biru di Z = -50.0 (Lebih jauh ke belakang/menjauh dari mata kita)
+    // glColor3f(1.0f, 1.0f, 1.0f);
+    // glBegin(GL_POLYGON);
+    //     glVertex3f( 20.0,  20.0, 0.0);
+    //     glVertex3f( 20.0, 120.0, 0.0);
+    //     glVertex3f(120.0, 120.0, 0.0);
+    //     glVertex3f(120.0,  20.0, 0.0);
+    // glEnd();
 
     glutSwapBuffers();
 }
 
 void reshape(int width, int height)
 {
-    if (height == 0)
-        height = 1;
+    if (height == 0) height = 1;
+    glViewport(0, 0, width, height);
 
-    glViewport(
-        0,
-        0,
-        width,
-        height
-    );
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    // ----------------------------------------------------
+    // EKSPERIMEN Z-AXIS (NEAR & FAR)
+    // Format: glOrtho(left, right, bottom, top, near, far)
+    // ----------------------------------------------------
+    
+    // Skenario 1: (Silakan comment Skenario 2, dan uncomment baris ini)
+    // Kotak merah (z=0) dan biru (z=-50) masuk ke rentang near (-100) sampai far (100)
+    // glOrtho(-100.0, 150.0, -100.0, 150.0, -100.0, 100.0);
+    
+    // Skenario 2: (Default kita coba yang ini dulu)
+    // Kotak merah kelihatan (z=0), tapi Kotak Biru (z=-50) HILANG!
+    // Kenapa hilang? Karena 'far' cuma mentok di 10.0 (nggak sampai 50.0).
+    glOrtho(0, 200.0, 0.0, 200.0, -1.0, 1.0);
+
+    glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv)
@@ -53,13 +98,13 @@ int main(int argc, char** argv)
     );
 
     glutInitWindowSize(
-        800,
+        600,
         600
     );
 
     glutInitWindowPosition(
-        100,
-        100
+        0,
+        0
     );
 
     glutCreateWindow(
